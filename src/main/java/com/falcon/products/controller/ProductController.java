f00824b9e289f4aaf9bb5c0386944183679a580e
MMrFalcon <kubasokol123456@gmail.com>
2019-10-07 23:13:03 +0200

    Testing rest request for get Product.

---
+++ src/main/java/com/falcon/products/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.falcon.products.controller;
+
+import com.falcon.products.domain.Product;
+import com.falcon.products.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    private final Logger log = LoggerFactory.getLogger(ProductController.class);
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id) {
+        log.debug("Rest request to get product with id : {}", id);
+        Product product = productService.getProductById(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+}
