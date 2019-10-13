1dc33c1061690c9a8b1b12402f2f5384051f4c6e
MMrFalcon <kubasokol123456@gmail.com>
2019-10-13 16:32:03 +0200

    Adding profiles and Repository annotations

--- src/main/java/com/falcon/products/repository/ProductRepository.java
+++ src/main/java/com/falcon/products/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.falcon.products.repository;
 
 import com.falcon.products.domain.Product;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
 }
--- src/main/java/com/falcon/products/repository/UserRepository.java
+++ src/main/java/com/falcon/products/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.falcon.products.repository;
 
 import com.falcon.products.domain.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findUserByLogin(String login);
 }
